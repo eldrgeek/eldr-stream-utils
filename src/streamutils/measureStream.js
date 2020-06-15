45157305d49c93d73065f2b45637830259665e69
Mike Wolf <mw@mike-wolf.com>
2020-06-14 22:52:25 -0400

    Fixed bug in measure stream

--- src/streamutils/measureStream.js
+++ src/streamutils/measureStream.js
@@ -6,9 +6,11 @@ export default function measureStream(stream) {
   video.height = 15;
   video.autoplay = true;
   video.srcObject = stream;
+  const span = document.createElement("span");
   setTimeout(() => {
     document.body.appendChild(canvas);
     document.body.appendChild(video);
+    document.body.appendChild(span);
   }, 1000);
 
   video.addEventListener("play", () => {
@@ -16,17 +18,19 @@ export default function measureStream(stream) {
   });
   let frameNo = 0;
   const update = () => {
-    if (frameNo++ > 11000) return;
+    frameNo++;
     ctx.drawImage(video, 0, 0, 40, 15);
-    let frame = ctx.getImageData(0, 0, 10, 10);
-    let len = frame.data.length;
+    let frame = ctx.getImageData(0, 0, 10, 10).data;
+    let len = frame.length;
     let colors = {};
     for (let i = 0; i < len; i += 4) {
       const color = (frame[i] * 16 + frame[i + 1]) * 16 + frame[i + 2];
       if (!colors[color]) colors[color] = 0;
       colors[color]++;
-      if (frameNo++ > 10000) console.log("Frame ", frameNo++, frame);
+
+      // if (frameNo % 1000 === 1) console.log("Frame ", frameNo, frame);
     }
+    if (frameNo % 1000 === 1) console.log("Frame ", frameNo, colors);
     requestAnimationFrame(update);
   };
 }
